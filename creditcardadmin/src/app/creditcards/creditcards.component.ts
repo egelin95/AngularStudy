bb6604520092ee57a44d06b958ff91fc504836ac
DESKTOP-3UDEAKE\Egelin <egelin.ellermaa@gmail.com>
2024-05-26 22:25:09 +0300

    seitsmes video

--- creditcardadmin/src/app/creditcards/creditcards.component.ts
+++ creditcardadmin/src/app/creditcards/creditcards.component.ts
@@ -4,95 +4,7 @@ import { MatTableDataSource } from '@angular/material/table';
 import { SelectionModel } from '@angular/cdk/collections';
 import {MatPaginator} from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
-
-const TABLE_DATA: CreditCard[] = [
-  {id: 1,
-    name: 'Bank of AMerica',
-    description: "Bank of America offers customers with various options",
-    bankName: "Bank of America",
-    maxCredit: 3000,
-    interestRate: 10,
-    active: true,
-    recommendedScore: '700-900',
-    annualFee: 4,
-    termsAndConditions: "Following are the tersm and conditions",
-    createdDate: '2024-26-05',
-    updatedDate: '2024-26-05'
-
-  },
-  {id: 2,
-    name: 'Bank of AMerica',
-    description: "Bank of America offers customers with various options",
-    bankName: "Bank of America",
-    maxCredit: 3000,
-    interestRate: 10,
-    active: true,
-    recommendedScore: '700-900',
-    annualFee: 4,
-    termsAndConditions: "Following are the tersm and conditions",
-    createdDate: '2024-26-05',
-    updatedDate: '2024-26-05'
-
-  },
-  {id: 3,
-    name: 'Bank of AMerica',
-    description: "Bank of America offers customers with various options",
-    bankName: "Bank of America",
-    maxCredit: 3000,
-    interestRate: 10,
-    active: true,
-    recommendedScore: '700-900',
-    annualFee: 4,
-    termsAndConditions: "Following are the tersm and conditions",
-    createdDate: '2024-26-05',
-    updatedDate: '2024-26-05'
-
-  },
-  {id: 2,
-    name: 'Bank of AMerica',
-    description: "Bank of America offers customers with various options",
-    bankName: "Bank of America",
-    maxCredit: 3000,
-    interestRate: 10,
-    active: true,
-    recommendedScore: '700-900',
-    annualFee: 4,
-    termsAndConditions: "Following are the tersm and conditions",
-    createdDate: '2024-26-05',
-    updatedDate: '2024-26-05'
-
-  },
-  {id: 2,
-    name: 'Bank of AMerica',
-    description: "Bank of America offers customers with various options",
-    bankName: "Bank of America",
-    maxCredit: 3000,
-    interestRate: 10,
-    active: true,
-    recommendedScore: '700-900',
-    annualFee: 4,
-    termsAndConditions: "Following are the tersm and conditions",
-    createdDate: '2024-26-05',
-    updatedDate: '2024-26-05'
-
-  },
-  {id: 2,
-    name: 'Bank of AMerica',
-    description: "Bank of America offers customers with various options",
-    bankName: "Bank of America",
-    maxCredit: 3000,
-    interestRate: 10,
-    active: true,
-    recommendedScore: '700-900',
-    annualFee: 4,
-    termsAndConditions: "Following are the tersm and conditions",
-    createdDate: '2024-26-05',
-    updatedDate: '2024-26-05'
-
-  },
-
-
-]
+import { CreditcardsService } from '../services/creditcards.service';
 
 
 @Component({
@@ -101,19 +13,34 @@ const TABLE_DATA: CreditCard[] = [
   styleUrls: ['./creditcards.component.scss']
 })
 export class CreditcardsComponent {
+  creditcards: CreditCard[] = [];
+
+  
+
+  constructor(private creditCardsService: CreditcardsService) {
+    this.creditCardsService.getCreditCards().subscribe((data:CreditCard[]) => {
+      this.creditcards = data;
+
+      this.dataSource = new MatTableDataSource(this.creditcards);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+
+      
+
+    })
+  }
+
+  dataSource = new MatTableDataSource(this.creditcards);
 displayColumns = ["select", "id",  "name","description", "bankName","maxCredit","interestRate","active", "recommendedScore"]
 
-dataSource = new MatTableDataSource(TABLE_DATA);
+
 
 selection = new SelectionModel(true, []);
 
 @ViewChild(MatPaginator) paginator!: MatPaginator;
 @ViewChild(MatSort) sort!:MatSort;
 
-ngAfterViewInit(){
-  this.dataSource.paginator =this.paginator;
-  this.dataSource.sort = this.sort;
-}
+
 
 selectHandler(row: CreditCard){
   this.selection.toggle(row as never);
